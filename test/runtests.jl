c1515fa4ff2d4786a546ac08878ac386f35b5d2e
Michael Myers <michael.myers@wustl.edu>
2025-09-01 17:52:02 -0500

    more tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,7 @@
 using GIFTI
 using Test
 using Pkg.Artifacts
+using GeometryBasics
 
 data_dir = artifact"msc_gifti_data_32k"
 filelist = readdir(data_dir)
@@ -10,11 +11,19 @@ filelist = readdir(data_dir)
 	for file in filelist
 		g = GIFTI.load("$data_dir/$file")
 		@test g isa GiftiStruct
+		for i in 1:length(g)
+			a = g[i]
+			@test data(a) isa AbstractArray
+		end
 		if has_pointset(g)
 			@test size(pointset(g)) == (32492, 3)
 		end
 		if has_triangle(g)
 			@test size(triangle(g)) == (32492 * 2 - 4, 3)
+		end
+		if has_pointset(g) && has_triangle(g)
+			mesh = GeometryBasics.Mesh(g)
+			@test mesh isa GeometryBasics.Mesh
 		end
 	end
 
