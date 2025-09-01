2df3e95541b7f9a693aa5d05fcc0765bca34fcb0
Michael Myers <michael.myers@wustl.edu>
2025-09-01 17:37:02 -0500

    added artifacts (gifti data from MSC01) and an acknowledgment in the readme

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,15 @@
 using GIFTI
 using Test
+using Pkg.Artifacts
+
+data_dir = artifact"msc_gifti_data_32k"
+filelist = readdir(data_dir)
 
 @testset "GIFTI.jl" begin
-    # Write your tests here.
+
+	for file in filelist
+		println(file)
+		g = GIFTI.load("$data_dir/$file")
+	end
+
 end
