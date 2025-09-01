fcfecfb446529264941c2cdfd44fe76d5219b78b
Michael Myers <michael.myers@wustl.edu>
2025-09-01 12:31:39 -0500

    added exports

--- src/GIFTI.jl
+++ src/GIFTI.jl
@@ -6,11 +6,22 @@ using Base64
 using GeometryBasics
 
 include("gifti_spec.jl")
+
 include("types.jl")
+export GiftiStruct, GiftiDataArray
+
 include("parsers.jl")
+
 include("io.jl")
+
 include("accessors.jl")
+export arrays, data, metadata, intent, intents
+export pointset, pointsets, triangle, triangles
+export has_pointset, has_triangle, has_time_series, has_timeseries
+
 include("conversion.jl")
+export Mesh
+
 include("show.jl")
 
 end
