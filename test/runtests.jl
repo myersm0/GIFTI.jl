afe2cfc10b0f6b5b7a9f4f67ad6508a4fa778748
Michael Myers <michael.myers@wustl.edu>
2025-09-01 17:46:25 -0500

    began adding some tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -8,8 +8,14 @@ filelist = readdir(data_dir)
 @testset "GIFTI.jl" begin
 
 	for file in filelist
-		println(file)
 		g = GIFTI.load("$data_dir/$file")
+		@test g isa GiftiStruct
+		if has_pointset(g)
+			@test size(pointset(g)) == (32492, 3)
+		end
+		if has_triangle(g)
+			@test size(triangle(g)) == (32492 * 2 - 4, 3)
+		end
 	end
 
 end
