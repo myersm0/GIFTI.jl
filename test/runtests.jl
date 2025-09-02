5d4055b4fc1e0e05bf7c9a5e8257c35e0335fb03
Michael Myers <michael.myers@wustl.edu>
2025-09-01 20:30:28 -0500

    more tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -11,20 +11,50 @@ filelist = readdir(data_dir)
 	for file in filelist
 		g = GIFTI.load("$data_dir/$file")
 		@test g isa GiftiStruct
+		@test metadata(g) isa Dict
+		@test data(g) isa Vector{GiftiDataArray}
+
 		for i in 1:length(g)
 			a = g[i]
+			@test metadata(a) isa GIFTI.ArrayMetadata
 			@test data(a) isa AbstractArray
+			@test size(a) == size(data(a)) == Tuple(metadata(a).dimensions)
+			@test intent(a) == intents(g)[i]
 		end
+
 		if has_pointset(g)
-			@test size(pointset(g)) == (32492, 3)
+			a = pointset(g)
+			@test intent(a) == "NIFTI_INTENT_POINTSET"
+			@test size(a) == (32492, 3)
 		end
+
 		if has_triangle(g)
-			@test size(triangle(g)) == (32492 * 2 - 4, 3)
+			a = triangle(g)
+			@test intent(a) == "NIFTI_INTENT_TRIANGLE"
+			@test size(a) == (32492 * 2 - 4, 3)
 		end
+
 		if has_pointset(g) && has_triangle(g)
 			mesh = GeometryBasics.Mesh(g)
 			@test mesh isa GeometryBasics.Mesh
 		end
+
+		if occursin(r".surf.gii$", file)
+			@test length(pointsets(g)) > 0
+			@test length(triangles(g)) > 0
+		end
+
+		if occursin(r".label.gii$", file)
+			label_table = metadata(g)["label_table"]
+			label_arrays = g["NIFTI_INTENT_LABEL"]
+			dtype = metadata(label_arrays[1]).data_type
+			@test label_table isa Dict{dtype, Tuple{String, Vector{Float32}}}
+			for label_array in label_arrays
+				array_vals = unique(data(label_array))
+				@test all(v in keys(label_table) for v in array_vals)
+			end
+		end
+
 	end
 
 end
