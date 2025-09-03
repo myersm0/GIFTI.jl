c766d0b92d1a71638023467334da3e9cad43f9e6
Michael Myers <120071679+myersm0@users.noreply.github.com>
2025-09-02 19:17:57 -0500

    Update conversion.jl

--- src/conversion.jl
+++ src/conversion.jl
@@ -7,7 +7,7 @@ function GeometryBasics.Mesh(g::GiftiStruct)
 	length(pointset_arrays) == 1 || @warn "Found multiple pointset arrays, using only the first."
 	length(triangle_arrays) == 1 || @warn "Found multiple triangle arrays, using only the first."
 
-	vert_array = data(pointset_arrays[1]) / 100
+	vert_array = data(pointset_arrays[1])
 	vertices = reshape(reinterpret(Point3f, vert_array'), (size(vert_array, 1),))
 	face_array = data(triangle_arrays[1])
 	faces = reinterpret(GLTriangleFace, vec(face_array'))
